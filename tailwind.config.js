c5cb24f897e6dec3017a5700d9c6a971deb28ac2
milon-hossain <abireshan32@gmial.com>
2024-02-17 19:10:35 +0600

    add header part

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-      extend: {},
+      extend: {
+        backgroundImage: {
+            'hero-pattern': "url('/img/hero-pattern.svg')",
+            'footer-texture': "url('/img/footer-texture.png')",
+          },
+      },
     },
     plugins: [],
   }
